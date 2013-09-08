80f8e9c35cd619ab7a6dd469847fe8c961191314
Matt Dahl <matt.dahl.2013@gmail.com>
2013-09-08 14:00:21 -0700

    Sets up templateCache backing.

---
+++ app/assets/javascripts/index_app_bootstrap.js
@@ -0,0 +1,27 @@
+var NN = NN || {};
+
+NN.Index = angular.module('index_app', ['index_app.controllers']);
+NN.Index.Controllers = angular.module('index_app.controllers', []);
+
+/**
+ * Provides configuration for the index module, namely setting up route forwarding using $routeProvider for a single-page app experience
+ */
+NN.Index.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/home',
+		{
+			templateUrl: 'partials/home.html',
+			controller: 'HomeController'
+		})
+		.when('/about',
+		{
+			templateUrl: 'partials/about.html',
+			controller: 'AboutController'
+		})
+		.when('/contact',
+		{
+			templateUrl: 'partials/contact.html',
+			controller: 'ContactController'
+		})
+		.otherwise({redirectTo: '/home'});
+}]);
