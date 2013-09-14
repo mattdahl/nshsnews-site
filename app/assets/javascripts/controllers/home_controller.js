a20d2d59945f1cbc2a79f21a6057686247f9d9c5
Matt Dahl <matt.dahl.2013@gmail.com>
2013-09-13 20:25:55 -0700

    Formats all five slides with proper transitions and positioning.

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -3,7 +3,6 @@
  */
 NN.Index.Controllers.controller('HomeController', ['$scope', function ($scope) {
 	// Sets up the slider
-	$status = $(".status");
 	var options = {
 	    autoPlay: false,
 	    autoPlayDelay: 4000,
@@ -17,31 +16,8 @@ NN.Index.Controllers.controller('HomeController', ['$scope', function ($scope) {
 	    animateStartingFrameIn: true,
 	    navigationSkipThreshold: 750,
 	    preventDelayWhenReversingAnimations: true,
-	    customKeyEvents: {
-	        80: "pause"
-	    }
+	    customKeyEvents: null
 	};
 
-	var sequence = $("#sequence").sequence(options).data("sequence");
-
-	sequence.afterNextFrameAnimatesIn = function () {
-	    if (sequence.settings.autoPlay && !sequence.hardPaused && !sequence.isPaused) {
-	        $status.addClass("active").css("opacity", 1);
-	    }
-	    $(".prev, .next").css("cursor", "pointer").animate({"opacity": 1}, 500);
-	};
-	sequence.beforeCurrentFrameAnimatesOut = function () {
-	    if (sequence.settings.autoPlay && !sequence.hardPaused) {
-	        $status.css({"opacity": 0}, 500).removeClass("active");
-	    }
-	    $(".prev, .next").css("cursor", "auto").animate({"opacity": 0.7}, 500);
-	};
-	sequence.paused = function () {
-	    $status.css({"opacity": 0}).removeClass("active").addClass("paused");
-	};
-	sequence.unpaused = function () {
-	    if (!sequence.hardPaused) {
-	        $status.removeClass("paused").addClass("active").css("opacity", 1);
-	    }
-	};
+	$("#sequence").sequence(options).data("sequence");
 }]);
